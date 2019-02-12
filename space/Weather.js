6071613819ff9a504ba6ba20f240e9f61f2b66b3
TTT00x <suga.mariko51089@gmail.com>
2019-02-12 17:42:44 +0900

    Add Animation

--- space/Weather.js
+++ space/Weather.js
@@ -1,43 +1,78 @@
 import React from 'react';
-import { View, ScrollView, Text, Image, Dimensions} from 'react-native';
+import { Animated, View, Text } from 'react-native';
 
 export default class Weather extends React.Component {
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      fadeAnim: new Animated.Value(0),
+      position: new Animated.ValueXY(0, -50),
+      twirl: new Animated.Value(0)
+    }
+  }
+  componentDidMount() {
+    const gestureState = {
+      vx: 10000,
+      vy: 10000
+    }
+    Animated.parallel([
+      Animated.timing(this.state.position, {
+        toValue: {x: 0, y: 20},
+        duration: 300,
+        delay: 200
+      }),
+      Animated.timing(this.state.fadeAnim, {
+        toValue: 1,
+        duration: 300,
+        delay: 200
+      }),
+    ]).start();
+  }
   render() {
     return (
       <View style={{
-        backgroundColor: 'white',
-        width: '90%',
-        marginBottom: 30
+        height: 100,
+        width: '100%',
+        marginBottom: 30,
       }}>
-        <View style={{
-          borderBottomWidth: 2,
-          borderBottomColor: 'gray',
-          paddingTop: 10,
-          paddingLeft: 20,
-          paddingRight: 20,
-          paddingBottom: 10,
-        }}>
-          <Text style={{
-            color: 'gray'
-          }}>Weather</Text>
-        </View>
-        <View style={{
-          height: 300,
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
+        <Animated.View style={{
           backgroundColor: 'white',
-          paddingTop: 10,
-          paddingLeft: 20,
-          paddingRight: 20,
-          paddingBottom: 10,
+          width: '90%',
+          opacity: this.state.fadeAnim,
+          position: 'absolute',
+          top: this.state.position.y,
+          left: '5%'
         }}>
-          <Text>City</Text>
-          <Text>Img</Text>
-          <Text>gra</Text>
-        </View>
+          <View style={{
+            display: 'flex',
+            justifyContent: 'center',
+            height: 50,
+            borderBottomWidth: 2,
+            borderBottomColor: 'gray',
+            paddingLeft: 20,
+            paddingRight: 20,
+          }}>
+            <Text style={{
+              color: 'gray'
+            }}>Weather</Text>
+          </View>
+          <View style={{
+            height: 50,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            paddingTop: 10,
+            paddingLeft: 20,
+            paddingRight: 20,
+            paddingBottom: 10,
+          }}>
+            <Text>City</Text>
+            <Text>Img</Text>
+            <Text>gra</Text>
+          </View>
+        </Animated.View>
       </View>
     )
   }
